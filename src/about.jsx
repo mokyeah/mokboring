19f3a4dc236f30d37536e9120221e03d4c4c128b
mokyeah <mzmmzm2008@gmail.com>
2023-02-04 13:55:40 +0800

    blog
    yes

--- src/about.jsx
+++ src/about.jsx
@@ -3,7 +3,7 @@ import { Icon } from "@iconify/react";
 function about() {
   return (
     <div className='justify-center items-center flex flex-col gap-8 w-full h-screen md:text-3xl'>
-        <div>You can learn about diffrent language in the world</div>
+        <div>Blog</div>
        
     </div>
   )
---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBU1EdBhFh7i4gQpWbbgY7RPVSM0e-4ny8",
+  authDomain: "blog-d81e0.firebaseapp.com",
+  projectId: "blog-d81e0",
+  storageBucket: "blog-d81e0.appspot.com",
+  messagingSenderId: "69039021647",
+  appId: "1:69039021647:web:34bbf855ab08b7934d543a",
+  measurementId: "G-XNRTWEVV00"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+ 
